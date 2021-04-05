026b3b771c2b10996a41f1ac06f2ac28a77706b5
sku1978 <shailesh_kmr@yahoo.com>
2021-04-05 09:21:17 +0100

    Update utils.py

--- Spark/lib/utils.py
+++ Spark/lib/utils.py
@@ -9,3 +9,21 @@ def get_spark_app_config():
   for (key, val) in config.items("SPARK_APP_CONFIGS"):
     spark_conf.set(key, val)
   return spark_conf
+
+def load_survey_df(spark, url):
+   spark.sparkContext.addFile(url)
+
+   survey_df=spark.read \
+   .format("csv") \
+   .option("header", "true") \
+   .option("inferSchema", "true") \
+   .load('file://'+SparkFiles.get("sample.csv"))
+
+   return survey_df
+
+def count_by_country(survey_df):
+  count_df= survey_df.select("Age", "Gender", "Country", "State") \
+                     .where("Age <= 40") \
+                     .groupBy("Country") \
+                     .count()
+  return count_df
